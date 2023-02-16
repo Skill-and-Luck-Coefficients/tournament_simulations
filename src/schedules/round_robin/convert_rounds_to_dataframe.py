9f369fcf01c3854a5fc944183a9339451c245ec5
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2023-02-16 18:54:32 -0300

    Add conversion from rounds to df

---
+++ src/schedules/round_robin/convert_rounds_to_dataframe.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+from .single_round_robin import Round
+
+
+def convert_list_of_rounds_to_dataframe(
+    rounds: list[Round], tournament_id: str
+) -> pd.DataFrame:
+
+    """
+    Converts a list of rounds into a dataframe.
+
+    ----
+    Parameters:
+
+        rounds: list[
+            tuple[  # Round: tuple of matches
+                tuple[int, int]  # Match: each team is a number
+            ]
+        ]
+            List of rounds.
+
+        tournament_id: str
+            Tournament name.
+
+    ----
+    Returns:
+
+        pd.DataFrame[
+            index=[
+                "id": tournament_id\n
+                "date number": which rounds the match belongs to
+            ],
+            columns=[
+                "home": home team (integer)\n
+                "away": away team (integer)
+            ]
+        ]
+    """
+
+    # don't need to set up the index, since exploded will already set all
+    # matches to have the same index (which is our date number)
+    exploded_rounds: pd.Series = pd.Series(rounds).explode()
+
+    iterables = [[tournament_id], exploded_rounds.index]
+    index = pd.MultiIndex.from_product(iterables, names=["id", "date number"])
+
+    return pd.DataFrame(
+        data=exploded_rounds.to_list(),
+        index=index,
+        columns=["home", "away"],
+    )
---
+++ src/tests/test_schedules/test_convert_rounds_to_dataframe.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+import schedules.round_robin.convert_rounds_to_dataframe as crd
+
+
+def test_convert_list_of_rounds_to_dataframe():
+
+    rounds = [((0, 1),), ((0, 2),), ((3, 0),), ((1, 0),)]
+    expected_cols = {
+        "id": ["id"] * 4,
+        "date number": [0, 1, 2, 3],
+        "home": [0, 0, 3, 1],
+        "away": [1, 2, 0, 0],
+    }
+    expected = pd.DataFrame(expected_cols).set_index(["id", "date number"])
+
+    rounds = [((0, 1), (2, 3)), ((0, 2), (1, 3)), ((3, 0), (2, 1)), ((1, 0), (3, 2))]
+    expected_cols = {
+        "id": ["id"] * 8,
+        "date number": [0, 0, 1, 1, 2, 2, 3, 3],
+        "home": [0, 2, 0, 1, 3, 2, 1, 3],
+        "away": [1, 3, 2, 3, 0, 1, 0, 2],
+    }
+    expected = pd.DataFrame(expected_cols).set_index(["id", "date number"])
+
+    assert crd.convert_list_of_rounds_to_dataframe(rounds, "id").equals(expected)
+
+    rounds = [((3, 2), (0, 1)), ((2, 0), (1, 3)), ((2, 1), (3, 0)), ((1, 0), (2, 3))]
+    expected_cols = {
+        "id": ["ok"] * 8,
+        "date number": [0, 0, 1, 1, 2, 2, 3, 3],
+        "home": [3, 0, 2, 1, 2, 3, 1, 2],
+        "away": [2, 1, 0, 3, 1, 0, 0, 3],
+    }
+    expected = pd.DataFrame(expected_cols).set_index(["id", "date number"])
+
+    assert crd.convert_list_of_rounds_to_dataframe(rounds, "ok").equals(expected)
