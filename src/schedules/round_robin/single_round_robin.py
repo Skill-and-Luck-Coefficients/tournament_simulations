5964d35b6daced89195b7c2b9ffa12fd2496d71f
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2023-02-25 20:29:27 -0300

    Fix documentation typo

--- src/schedules/round_robin/single_round_robin.py
+++ src/schedules/round_robin/single_round_robin.py
@@ -56,9 +56,9 @@ class SingleRoundRobin:
             -> Then full_schedule(num_schedules = 2) can produce something like:
 
                 [
-                                                # first double round-robin\n
+                                                # first single round-robin\n
                     ( ((1,0), (2,3)), ((1,2), (3,0)), ((2,0), (1,3)) ),\n
-                                                # second double round-robin\n
+                                                # second single round-robin\n
                     ( ((2,0), (1,3)), ((1,0), (2,3)), ((1,2), (3,0)) ),
                 ]
 
