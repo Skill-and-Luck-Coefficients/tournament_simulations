6d7bbdc814ea92d9255507cb127df00ad7c1809b
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2023-02-25 23:41:18 -0300

    Add Team to __init__ file

--- src/schedules/round_robin/utils/__init__.py
+++ src/schedules/round_robin/utils/__init__.py
@@ -1,10 +1,11 @@
 from .convert_rounds_to_dataframe import convert_list_of_rounds_to_dataframe
 from .rename_teams import rename_teams_in_rounds
-from .types import Match, Round
+from .types import Match, Round, Team
 
 __all__ = [
     "convert_list_of_rounds_to_dataframe",
     "rename_teams_in_rounds",
+    "Team",
     "Match",
     "Round",
 ]
